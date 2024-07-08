40719dcbfae1b3ecd876bbc7fc00358ff4ff230d
gabriele <gabriele.cuccu@gmail.com>
2024-07-08 12:20:08 +0100

    fixed error in main.go

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ func main() {
 	game := newGame()
 	game.newMove(0, 0, computerPlayer)
 	game.newMove(2, 2, computerPlayer)
-	fmt.Println(game.canComputerWin())
+	fmt.Println(game.canWin(computerPlayer))
 	fmt.Println(game)
 }
